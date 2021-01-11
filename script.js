c46d823aa364404e14bcb277b08ab7a0de47a41c
kozmarti <kozmarti@gmail.com>
2021-01-11 13:53:11 +0100

    quete done

---
+++ script.js
@@ -0,0 +1,19 @@
+function fetchSimpsonQuote() {
+    const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+    axios.get(url)
+        .then(function(response) {
+            return response.data[0];
+        })
+        .then(function(simpson) {
+            console.log('data decoded from JSON:', simpson);
+            console.log(simpson.quote);
+            const simpsonHtml = `
+        <h2>${simpson.character}</h2>
+        <p><strong>${simpson.quote}</strong></p>
+        <img src="${simpson.image}" />
+        `;
+            console.log(simpsonHtml);
+            document.getElementById('simpson-quote').innerHTML = simpsonHtml;
+        });
+}
+fetchSimpsonQuote();
